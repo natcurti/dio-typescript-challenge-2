b02ade8a66a31d5426cfdf0d142de2006dc6927b
natcurti <naticurti01@gmail.com>
2024-01-10 11:19:20 -0300

    Start project Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/700.css';
+import { 
+  ChakraProvider,
+  Flex, 
+  Box, 
+  Text,
+  Input,
+  InputRightElement,
+  InputGroup,
+  Button,
+  Link
+} from '@chakra-ui/react';
+import {EmailIcon, LockIcon} from '@chakra-ui/icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+        <Flex minHeight='100vh' width='100%' backgroundColor='#4111A1' justifyContent='center' alignItems='center'>
+          <Flex flexDirection='column' width='30%' backgroundColor='#FFF' borderRadius='0.5rem' 
+          padding='2rem' gap='1rem'boxShadow='15px 15px 30px -15px rgba(0,0,0,0.85)'>
+            <Box color='#112D43' fontSize='2rem' fontWeight='700' fontFamily="'Open Sans', sans-serif">
+              <h1>Login:</h1>
+            </Box>          
+            <Text color='#A1A1A3' fontFamily="'Open Sans', sans-serif" fontSize='1rem'>
+              Digite seus dados nos campos abaixo.
+            </Text>
+            <Flex flexDirection='column' gap='0.5rem' fontFamily="'Open Sans', sans-serif" fontWeight='700'>
+              <label htmlFor='inputEmail'>Email: </label>
+              <InputGroup size='lg' fontWeight='400'> 
+                <Input color='#112D43' fontSize='1rem' placeholder="Digite seu email" _placeholder={{color: '#A1A1A3', fontSize:'1rem' }} id='inputEmail'/>
+                <InputRightElement pointerEvents="none">
+                  <EmailIcon color='#112D43'/>
+                </InputRightElement>
+              </InputGroup>
+            </Flex>
+            <Flex flexDirection='column' gap='0.5rem' fontFamily="'Open Sans', sans-serif" fontWeight='700'>
+              <label htmlFor='inputPassword'>Senha:</label>
+              <InputGroup size='lg' fontWeight='400'>
+                <Input color='#112D43' fontSize='1rem' placeholder="Digite sua senha"  _placeholder={{color: '#A1A1A3', fontSize:'1rem' }} id='inputPassword'/>
+                <InputRightElement pointerEvents="none">
+                <LockIcon color='#112D43'/>
+                </InputRightElement>
+              </InputGroup>
+            </Flex>
+            <Link color='#A1A1A3' fontFamily="'Open Sans', sans-serif" href='#'>Esqueci minha senha</Link>
+            <Button backgroundColor='#E33F84' _hover={{ bg: '#ff87b9' }} color='#FFF' fontFamily="'Open Sans', sans-serif" fontWeight='700'>Enviar</Button>
+          </Flex>
+        </Flex>
+    </ChakraProvider>
   );
 }
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="react-scripts" />
+declare module "@chakra-ui/react"
+
+
